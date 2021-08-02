4e085c7f4cc18ca9f02ba46174d7bfc34483e85b
sreavis07 <sean_reavis@cable.comcast.com>
2021-08-02 12:58:57 -0400

    Still working on Query 4

--- queries.sql
+++ queries.sql
@@ -1,7 +1,17 @@
 ## Part 1: Test it with SQL
+
 Select * from techjobs.job;
+
 ## Part 2: Test it with SQL
 
+Select name
+
+from techjobs.employer
+
+where (location = "Saint Louis");
+
 ## Part 3: Test it with SQL
+
+TRUNCATE techjobs.job;
 
 ## Part 4: Test it with SQL
